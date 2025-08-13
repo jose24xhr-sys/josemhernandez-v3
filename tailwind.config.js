af0e820692048b45c9d7349db031e9d3338fe4c4
jose24xhr-sys <jose24xhr@gmail.com>
2025-08-13 11:38:43 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+  darkMode: 'class',
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+}
